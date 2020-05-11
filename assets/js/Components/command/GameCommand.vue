2f7c37702d22ee1f07027eac63416f4647017782
Benitorax <sdkyo@hotmail.com>
2020-05-11 23:09:06 +0200

    Add animation when roll dice in game step

--- assets/js/Components/command/GameCommand.vue
+++ assets/js/Components/command/GameCommand.vue
@@ -2,7 +2,9 @@
     <div>
         <div style="height: 300px">
             <div style="margin-bottom: 25px">
-                <span v-for="diceClass in diceIcons" :key="diceClass" class="has-margin-right-7" :class="diceClass"><i class="fas fa-cube fa-3x"></i></span>    
+                <transition-group name="dice-icon" tag="span">
+                    <span v-for="diceClass in diceIcons" :key="diceClass" class="has-margin-right-7 dice-icon-item" :class="diceClass"><i class="fas fa-cube fa-3x"></i></span>    
+                </transition-group>
             </div>
             <transition name="commands">
                 <div v-if="isShowed" class="buttons columns">
@@ -22,7 +24,9 @@
         </div>
         <div>
             <div style="margin-bottom: 25px">
-                <span v-for="index in (3-blackDiceCount)" :key="index" class="has-margin-right-7"><i class="fas fa-cube fa-3x"></i></span>    
+                <transition-group name="dice-icon" tag="span">
+                    <span v-for="diceClass in blackDiceIcons" :key="diceClass" class="has-margin-right-7 dice-icon-item" :class="diceClass"><i class="fas fa-cube fa-3x"></i></span>    
+                </transition-group>
             </div>
             <div v-if="isShowed" class="columns">
                 <div v-if="blackDiceCount < 3 && diceLeft > 0 && diceCount > 0" class="column is-full button-audience">
@@ -43,7 +47,9 @@
                 diceCount: 0, // whiteDice isn't counted
                 blackDiceCount: 0,
                 whiteDiceCount: 0,
-                isShowed: true
+                isShowed: true,
+                diceIcons: null,
+                blackDiceIcons: ['icon-black', 'icon-black', 'icon-black']
             }
         },
         computed: {
@@ -71,15 +77,6 @@
             },
             diceLeft() {
                 return 6 - (this.diceCount + this.whiteDiceCount);
-            },
-            diceIcons(diceLeft) {
-                let icons = ['icon-yellow', 'icon-orange', 'icon-red', 'icon-violet', 'icon-blue', 'icon-dark-blue'];
-                if(this.mode === 'normal') return icons.slice(this.diceCount);
-                else {
-                    if(this.whiteDiceCount === 0) icons[5] = 'icon-white';
-                    else icons.slice(0, -1);
-                    return icons.slice(this.diceCount + this.whiteDiceCount);
-                }  
             }
         },
         methods: {
@@ -87,6 +84,7 @@
                 this.isShowed = false;
                 let color = this.defineColor();
                 this.diceCount += 1;
+                this.removeOneDiceIcon();
                 Ajax.get('dice/'+ color).then(({data}) => {
                     Event.$emit('message:add', { 
                         message: data.message, 
@@ -99,6 +97,7 @@
             throwWhiteDice() {
                 this.isShowed = false;
                 this.whiteDiceCount += 1;
+                this.removeWhiteDiceIcon();
                 Ajax.get('dice/white').then(({data}) => {
                     Event.$emit('message:add', { 
                         message: data.message, 
@@ -140,17 +139,34 @@
                 Event.$emit('parameters:update', { step: 'rate' });
             },
             throwBlackDiceEvent() {
-                if(this.step === 'play' && this.blackDiceCount < 3) {
+                if(this.step === 'play' && this.blackDiceCount < 3 && this.diceCount > 0) {
+                    this.removeBlackDiceIcon();
                     if(this.diceCount === 0) return;
                     if(this.mode === 'normal' && this.diceCount <= 5) this.throwBlackDice();
                     else if(this.mode === 'whiteDice' && (this.diceCount + this.whiteDiceCount) <= 5) this.throwBlackDice();
                 }
+            },
+            removeOneDiceIcon() {
+                this.diceIcons.splice(-1,1);
+            },
+            removeWhiteDiceIcon() {
+                this.diceIcons.splice(0,1);
+            },
+            removeBlackDiceIcon() {
+                this.blackDiceIcons.splice(-1,1);
+            },
+            hydrateDiceIcons(diceLeft) {
+                let iconClasses = ['icon-dark-blue', 'icon-blue', 'icon-violet', 'icon-red', 'icon-orange', 'icon-yellow' ];
+                if(this.mode === 'whiteDice') iconClasses[0] = 'icon-white';
+ 
+                this.diceIcons = iconClasses
             }
         },
         mounted() {
             Event.$emit('message:reset');
             Event.$emit('message:add', { message: this.subject, iconClass: 'comment-dots', colorClass: null });
             Event.$on('event:spacebar', () => this.throwBlackDiceEvent());
+            this.hydrateDiceIcons();
         },
 
     };
@@ -189,5 +205,41 @@
     }
     .icon-white {
         color: gainsboro;
+    }
+    .dice-icon-item {
+        display: inline-block;
+    }
+    .dice-icon-enter-active/*, .dice-icon-leave-active*/ {
+       transition: all 1s;
+    }
+    .dice-icon-enter {
+        opacity: 0;
+        transform: translateY(30px);
+    }
+    /*.dice-icon-leave-to {
+        transform: scale(6) translateX(-100px) rotate(-340deg);
+        opacity: 0.2;
+    }*/
+
+    .dice-icon-leave-active {
+        animation: roll-dice 1s ease-in-out;
+    }
+
+    @keyframes roll-dice {
+        from {
+            opacity: 1;
+        }
+        80% {
+            opacity: 0.5;
+            transform: scale(6) translateX(-110px) rotate(-680deg);
+        }
+        90% {
+            opacity: 0.2;
+            transform: scale(6) translateX(-110px) rotate(-680deg);
+        }
+        to {
+            opacity: 0;
+            transform: scale(6) translateX(-110px) rotate(-680deg);
+        }
     }
 </style>
